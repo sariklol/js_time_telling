fff5b187946f195021c0c25a779a4ec914487975
Sardor <tuxtasinovsardor5@gmail.com>
2025-07-09 22:57:01 +0500

    uploading

---
+++ script.js
@@ -0,0 +1,27 @@
+let time = +prompt('Введите время.')
+
+switch (true) {
+    case (time >= 0 && time <= 6):
+        alert(time + ' часа ночи')
+        break
+
+    case (time >= 7 && time <= 11):
+        alert(time + ' часов утра')
+        break
+
+    case (time === 12):
+        alert(time + ' часов дня')
+        break
+
+    case (time >= 13 && time <= 17):
+        alert(time - 12 + ' часа дня')
+        break
+
+    case (time >= 18 && time <= 23):
+        alert(time - 12 + ' часов вечера')
+        break
+
+    default:
+        alert('Что то пошло не так друг мой.')
+        break
+}
