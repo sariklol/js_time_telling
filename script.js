8eb644401091f142d7950ba93e20c2ba4afc6384
Sardor <tuxtasinovsardor5@gmail.com>
2025-07-09 23:05:15 +0500

    fixed-issue

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let time = +prompt('Введите время.')
+let time = +prompt('Введите время от 0 до 24.')
 
 switch (true) {
     case (time >= 0 && time <= 6):
         alert(time + ' часа ночи')
+        break
+
+    case (time === 24):
+        alert(time - 24 + " часов ночи")
         break
 
     case (time >= 7 && time <= 11):
